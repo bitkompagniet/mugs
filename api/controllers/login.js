10244b5dc1e04aa7235ecf612e8e6aa8694df76b
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2016-12-06 15:00:58 +0100

    Fix the login flow

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -2,13 +2,21 @@ const jsonwebtoken = require('jsonwebtoken');
 const tokenExpiry = require('../../lib/token-expiry');
 
 module.exports = function(store) {
-	return function(req, res) {
-		store.login(req.body.email, req.body.password)
-			.then(user => ({
+	return async function(req, res) {
+		if (!(req.body.email && req.body.password)) {
+			return res.failure('E-mail and password required.', 400);
+		}
+
+		try {
+			const user = await store.login(req.body.email, req.body.password);
+			const token = {
 				token: jsonwebtoken.sign(Object.assign(user, { exp: tokenExpiry() }), req.configuration('secret')),
 				user,
-			}))
-			.then(res.success)
-			.catch(err => res.failure(err, 401));
+			};
+
+			return res.success(token);
+		} catch (e) {
+			return res.failure('Invalid credentials.', 401);
+		}
 	};
 };
