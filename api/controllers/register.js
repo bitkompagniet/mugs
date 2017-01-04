70a8b3d37e845650e91f45fabd05fb9a2ed95a69
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-01-04 16:25:13 +0100

    Move first/last/fullname logic to middleware

--- api/controllers/register.js
+++ api/controllers/register.js
@@ -2,46 +2,40 @@ const rumor = require('rumor')('mugs:api:register');
 const _ = require('lodash');
 const mail = require('../../lib/mail');
 const urlJoin = require('url-join');
+const ensureFirstLastname = require('../middleware/ensureFirstLastname');
 
 module.exports = function(store) {
-	return async function(req, res, next) {
-		try {
-			const acceptedBody = _.pick(req.body, [
-				'email',
-				'fullname',
-				'firstname',
-				'lastname',
-				'password',
-				'data',
-			]);
+	return [
+		ensureFirstLastname(),
 
-			if ('fullname' in acceptedBody) {
-				if ('firstname' in acceptedBody || 'lastname' in acceptedBody) {
-					return res.failure('Either specify fullname or a firstname/lastname pair. Do not supply fullname in conjunction with any of the latter.', 422);
-				}
+		async function(req, res, next) {
+			try {
+				const acceptedBody = _.pick(req.body, [
+					'email',
+					'firstname',
+					'lastname',
+					'password',
+					'data',
+				]);
 
-				const split = acceptedBody.fullname.split(' ');
-				acceptedBody.firstname = split.slice(0, -1);
-				acceptedBody.lastname = split.slice(-1);
-				delete acceptedBody.fullname;
+				const user = await store.create(acceptedBody);
+				const rawUser = await store.getRaw(user._id);
+				await store.addRole(user._id, 'admin', `users/${user._id}`);
+				await store.addRole(user._id, 'member', `users/${user._id}`);
+
+				const appUrl = req.configuration('appUrl');
+				const mountPath = _.isArray(req.app.mountpath) ? req.app.mountpath[0] : req.app.mountpath;
+				const confirmRegistrationUrl = urlJoin(appUrl, mountPath, '/register/', rawUser.confirmationToken);
+
+				rumor.debug(`Confirm registration URL: ${confirmRegistrationUrl}`);
+				await mail.confirmation(_.merge({}, user, req.configuration(), { confirmRegistrationUrl }));
+
+				const updatedUser = await store.get(user._id);
+				return res.success(updatedUser);
+			} catch (e) {
+				return next(e);
 			}
+		},
 
-			const user = await store.create(acceptedBody);
-			const rawUser = await store.getRaw(user._id);
-			await store.addRole(user._id, 'admin', `users/${user._id}`);
-			await store.addRole(user._id, 'member', `users/${user._id}`);
-
-			const appUrl = req.configuration('appUrl');
-			const mountPath = _.isArray(req.app.mountpath) ? req.app.mountpath[0] : req.app.mountpath;
-			const confirmRegistrationUrl = urlJoin(appUrl, mountPath, '/register/', rawUser.confirmationToken);
-
-			rumor.debug(`Confirm registration URL: ${confirmRegistrationUrl}`);
-			await mail.confirmation(_.merge({}, user, req.configuration(), { confirmRegistrationUrl }));
-
-			const updatedUser = await store.get(user._id);
-			return res.success(updatedUser);
-		} catch (e) {
-			return next(e);
-		}
-	};
+	];
 };
---
+++ api/middleware/ensureFirstLastname.js
@@ -0,0 +1,18 @@
+const _ = require('lodash');
+
+module.exports = function() {
+	return function(req, res, next) {
+		if (req.body.fullname) {
+			if (req.body.firstname || req.body.lastname) {
+				return res.failure('Either specify fullname or a firstname/lastname pair. Do not supply fullname in conjunction with any of the latter.', 422);
+			}
+
+			const split = req.body.fullname.split(' ').filter(i => _.isString(i) && !_.isEmpty(i));
+			req.body.firstname = split.slice(0, -1);
+			req.body.lastname = split.slice(-1);
+			delete req.body.fullname;
+		}
+
+		return next();
+	};
+};
