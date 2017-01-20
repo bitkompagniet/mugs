dfa672b42ca7afe8b2ce45bcd455b8b1098099da
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2017-01-20 19:32:38 +0100

    fixes #23

--- api/controllers/addRoleToUser.js
+++ api/controllers/addRoleToUser.js
@@ -1,8 +1,10 @@
+const requireAuthentication = require('../middleware/requireAuthentication');
+
 module.exports = function(store) {
-	return function(req, res) {
+	return [requireAuthentication(), async function(req, res) {
 		const id = req.params.id;
 		if (!req.identity.is(`admin@users/${id}`)) return res.failure('You do not have permissions to modify this object.', 403);
-		store.addRole(id, req.body.role, req.body.scope);
-		return res.success();
-	};
+		const statusCode = await store.addRole(id, req.body.role, req.body.scope);
+		return res.success(null, statusCode);
+	}];
 };
--- store/query/addRole.js
+++ store/query/addRole.js
@@ -1,13 +1,23 @@
 const mercutio = require('mercutio');
 
 module.exports = async function (_id, role, scope) {
+	const user = await this.findById(_id);
+	const oldRolesLength = user.roles.length;
+
 	await this.findOneAndUpdate({ _id }, {
 		$addToSet: { roles: { role, scope } },
 	});
-	const user = await this.findById(_id);
-	const newRoles = user.roles;
+
+	const newUser = await this.findById(_id);
+
+	const newRoles = newUser.roles;
 	const m = mercutio(newRoles);
 	const rationalized = m.rationalize();
-	user.roles = rationalized.toArray();
-	return true;
+	newUser.roles = rationalized.toArray();
+	newUser.save();
+
+	if (newUser.roles.length > oldRolesLength) {
+		return 201; // 201: created
+	}
+	return 200;
 };
