aa30fa29ec9376b3037580cd32af71488b8892ee
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-01-24 18:32:55 +0100

    Add roles from role array on post user

--- store/query/addRole.js
+++ store/query/addRole.js
@@ -1,5 +1,5 @@
-module.exports = async function (_id, role, scope) {
-	return await this.findOneAndUpdate({ _id }, {
+module.exports = function (_id, role, scope) {
+	return this.findOneAndUpdate({ _id }, {
 		$addToSet: { roles: { role, scope } },
 	});
 };
