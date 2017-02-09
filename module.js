8aa4ce86847f5705f4e7dd7ff9e5bdb0b6d45cc0
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2017-02-09 16:01:34 +0100

    Fixes #42

--- module.js
+++ module.js
@@ -1,9 +1,13 @@
 const api = require('./api');
 const createStore = require('./store');
 const getConfig = require('./get-config');
+const rumor = require('rumor')('mugs');
 
 module.exports = function(args) {
 	const config = getConfig(args);
+	if (config.smtp) {
+		rumor.warn('smtp has not been set as ENV, e-mail sending will be supressed');
+	}
 	const store = createStore(config.db);
 	return api(store, config);
 };
--- run-server.js
+++ run-server.js
@@ -4,8 +4,13 @@ const getConfig = require('./get-config');
 const api = require('./api');
 const createStore = require('./store');
 const server = require('./server');
+const rumor = require('rumor')('Mugs');
 
 const config = getConfig(process.env);
+
+if (!config.smtp) {
+	rumor.warn('SMTP has not been specified as ENV, e-mail sending will be supressed.');
+}
 
 const store = createStore(config.db);
 server(api(store, config));
