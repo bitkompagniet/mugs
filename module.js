1d03833fb3e98df0b4257ae34ea6e9749e69ba76
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2017-02-09 16:04:44 +0100

    fix typo

--- module.js
+++ module.js
@@ -6,7 +6,7 @@ const rumor = require('rumor')('mugs');
 module.exports = function(args) {
 	const config = getConfig(args);
 	if (!config.smtp) {
-		rumor.warn('smtp has not been set as ENV, e-mail sending will be supressed');
+		rumor.warn('SMTP has not been set as ENV, e-mail sending will be supressed');
 	}
 	const store = createStore(config.db);
 	return api(store, config);
