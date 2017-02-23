69a792a5fc99d2d1b464af7f3d8ab9284d254593
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2017-02-23 13:51:03 +0100

    Try to use secret ENV var if set

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,7 @@ const decode = require('./decode');
 program
 	.version(version)
 	.option('-c, --clean', 'Suppress default roles when signing a token')
-	.option('-s, --secret <secret>', 'Define the secret used. Will default to "ssh".')
+	.option('-s, --secret <secret>', 'Define the secret used. Will default to ENV var $secret if set, otherwise "ssh".')
 	.option('-a, --admin', 'Add superadmin role to the signing (*@/)')
 	.option('-v, --verbose', 'Include header and signature when decoding a token.')
 	.description('mugs CLI tool.');
@@ -14,7 +14,7 @@ program
 program
 	.command('sign [roles...]')
 	.description('Sign a new token')
-	.action((roles, cmd) => sign(roles, cmd.parent.clean, cmd.parent.admin, cmd.parent.secret));
+	.action((roles, cmd) => sign(roles, cmd.parent.clean, cmd.parent.admin, cmd.parent.secret || process.env.secret));
 
 program
 	.command('decode')
