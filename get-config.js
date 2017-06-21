5d678ec4ff2227329964bb2f38bfcc74c8891999
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2017-06-21 18:00:01 +0200

    Fix #72 No unit test.

--- get-config.js
+++ get-config.js
@@ -19,6 +19,7 @@ module.exports = function(source) {
 		appUrl: optional('appUrl', source, 'http://localhost:3000/'),
 		redirectConfirmUrl: optional('redirectConfirmUrl', source, 'http://localhost:3000/redirect'),
 		port: optional('port', 3000),
+		unconfirmedLogins: optional('unconfirmedLogins', source, true),
 	};
 
 	return config;
