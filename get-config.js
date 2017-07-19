f543e2f3ba97df4d3fa8a544237235e9c0eedb45
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2017-07-19 13:44:05 +0200

    fix #80

--- get-config.js
+++ get-config.js
@@ -18,7 +18,7 @@ module.exports = function(source) {
 		appName: optional('appName', source, 'MUGS TEST INSTANCE'),
 		appUrl: optional('appUrl', source, 'http://localhost:3000/'),
 		redirectConfirmUrl: optional('redirectConfirmUrl', source, 'http://localhost:3000/redirect'),
-		port: optional('port', 3000),
+		port: required('port', source, 3000),
 	};
 
 	return config;
